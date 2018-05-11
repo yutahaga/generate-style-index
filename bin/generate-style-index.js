94430ba533904711243d43627f5b44b58a5f43a1
Yuta Haga <yutahaga7@gmail.com>
2018-05-11 16:30:47 +0900

    --verbose オプションの出力にANSIカラーを適用.

--- bin/generate-style-index.js
+++ bin/generate-style-index.js
@@ -5,6 +5,18 @@ const yargs = require('yargs');
 const finder = require('find-package-json');
 const generateStyleIndex = require('../lib/generate-style-index');
 
+const ansi = {
+  black: '\u001b[30m',
+  red: '\u001b[31m',
+  green: '\u001b[32m',
+  yellow: '\u001b[33m',
+  blue: '\u001b[34m',
+  magenta: '\u001b[35m',
+  cyan: '\u001b[36m',
+  white: '\u001b[37m',
+  reset: '\u001b[0m',
+};
+
 const argv = yargs
   .alias('p', 'path')
   .alias('s', 'syntax')
@@ -16,7 +28,9 @@ const syntax = argv.syntax || 'css';
 const eventHandlers = argv.verbose
   ? {
       file(filepath) {
-        console.log(filepath);
+        console.log(
+          `${ansi.green}[Generated] ${ansi.blue}${filepath}${ansi.reset}`
+        );
       },
       before() {
         console.log('Generating styles index files...');
